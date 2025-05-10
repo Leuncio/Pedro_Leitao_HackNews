3aed85be7a41abd18bbe021bc74f3ecbc508f6e6
Leuncio <leitaoshp@gmail.com>
2025-05-10 18:32:22 +0200

    scrap data de acuerdo con el final de la URL en "scraper.py"

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,16 @@
 # views.py
 
 from flask import Blueprint, request, render_template, url_for, redirect, flash, session
+from scraper import scrape_website
+
 
 # Create a Blueprint for the main application
 main_bp = Blueprint('main', __name__, template_folder='templates')  # Define blueprint with the name 'main'
 
-@main_bp.route('/', methods=['GET', 'POST'])
-def home():
+
+@main_bp.route('/<number_of_articles>', methods=['GET', 'POST'])
+def home(number_of_articles):
+    """ This function will increase the number of articles to scrape """
+
 
     return "Hello, World!"  # Render the home page with a simple message)
--- app/scraper.py
+++ app/scraper.py
@@ -1,1 +1,26 @@
 # scraper.py
+
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://news.ycombinator.com/"
+NUM = "?p=3"
+
+
+def number_of_articles(URL, NUM):
+    """ This function will increase the number of articles to scrape """
+
+    response = requests.get(URL + NUM)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, "html.parser")
+        titles = soup.find_all("span", class_="titleline")
+
+        for tag in titles:
+            link = tag.find("a")  # isso pega o <a href="...">
+            print(link.text, "→", link["href"])
+
+    else:
+        print("Failed to fetch the page.")   
+        print("Status code:", response.status_code)
+    
+number_of_articles(URL, NUM)
